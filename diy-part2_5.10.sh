78ed249f166277c279afc8e86fca88161e09bcdb
ywt114 <58467878+ywt114@users.noreply.github.com>
2022-10-30 11:00:20 +0800

    Update diy-part2_5.10.sh

--- diy-part2_5.10.sh
+++ diy-part2_5.10.sh
@@ -26,6 +26,7 @@ echo "$(date +'%m.%d.%Y')" > package/base-files/files/etc/openwrt_version
 
 # 修改部分默认设置
 sed -i "s/check_signature 1/check_signature 0/g" package/system/opkg/files/opkg-smime.conf
+echo "option check_signature 0" >> package/system/opkg/files/opkg.conf
 sed -i "/exit 0/d" package/lean/default-settings/files/zzz-default-settings
 echo "echo 'src/gz openwrt_kiddin9 https://op.supes.top/packages/x86_64' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
 echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
