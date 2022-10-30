9a59338084141c1be5eaf7eacc0adecb28bfb2d3
ywt114 <58467878+ywt114@users.noreply.github.com>
2022-10-30 15:11:52 +0800

    Update diy-part2_5.10.sh

--- diy-part2_5.10.sh
+++ diy-part2_5.10.sh
@@ -25,8 +25,10 @@ sed -i '/(<%=pcdata(ver.luciversion)%>)/a\      built by ywt114' package/lean/au
 echo "$(date +'%m.%d.%Y')" > package/base-files/files/etc/openwrt_version
 
 # 修改部分默认设置
-sed -i "s/check_signature 1/check_signature 0/g" package/system/opkg/files/opkg-smime.conf
-echo "option check_signature 0" >> package/system/opkg/files/opkg.conf
+sed -i "/signature/d" package/system/opkg/files/opkg-smime.conf
+echo "# option check_signature" >> package/system/opkg/files/opkg-smime.conf
+sed -i "/signature/d" package/system/opkg/files/opkg.conf
+echo "# option check_signature" >> package/system/opkg/files/opkg.conf
 sed -i "/exit 0/d" package/lean/default-settings/files/zzz-default-settings
 echo "echo 'src/gz openwrt_kenzok8 https://op.dllkids.xyz/packages/x86_64' >> etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
 echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
