fdece5536314d70de05d5c616a2c471d0745e917
ywt114 <58467878+ywt114@users.noreply.github.com>
2022-10-30 09:51:44 +0800

    Update diy-part2_5.10.sh

--- diy-part2_5.10.sh
+++ diy-part2_5.10.sh
@@ -26,7 +26,9 @@ echo "$(date +'%m.%d.%Y')" > package/base-files/files/etc/openwrt_version
 
 # 修改部分默认设置
 sed -i "s/check_signature 1/check_signature 0/g" package/system/opkg/files/opkg-smime.conf
+sed -i "/exit 0/d" package/lean/default-settings/files/zzz-default-settings
 echo "echo 'src/gz openwrt_kiddin9 https://op.supes.top/packages/x86_64' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
+echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
 sed -i "s/mirrors.cloud.tencent.com\/lede/mirrors.cloud.tencent.com\/openwrt/g" package/lean/default-settings/files/zzz-default-settings
 sed -i "s/sed -i 's\/root::0:0:99999:7:::/# sed -i 's\/root::0:0:99999:7:::/g" package/lean/default-settings/files/zzz-default-settings
 sed -i "s/sed -i '\/REDIRECT --to-ports/# sed -i '\/REDIRECT --to-ports/g" package/lean/default-settings/files/zzz-default-settings
