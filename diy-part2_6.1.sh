82a32346ddb9ed8bb99d6a72f915ee0dbba3ac59
ywt114 <58467878+ywt114@users.noreply.github.com>
2024-01-19 22:21:09 +0800

    Update diy-part2_6.1.sh

--- diy-part2_6.1.sh
+++ diy-part2_6.1.sh
@@ -3,25 +3,26 @@
 #!/bin/bash
 
 function merge_package() {
-    # 参数1是分支名, 参数2是库地址。参数3是所有文件下载到指定路径。
+    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
     # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
     if [[ $# -lt 3 ]]; then
-        echo "Syntax error: [$#] [$*]" >&2
-        return 1
-    fi
-
+    	echo "Syntax error: [$#] [$*]"
+    	return 0
+	fi
     trap 'rm -rf "$tmpdir"' EXIT
-
-    git clone -b "$1" --depth 1 --filter=blob:none --sparse "$2" "$tmpdir"
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
     cd "$tmpdir"
     git sparse-checkout init --cone
-    git sparse-checkout set "${@:3}"
-
+    git sparse-checkout set "$@"
     # 使用循环逐个移动文件夹
-    for folder in "${@:3}"; do
+    for folder in "$@"; do
         mv -f "$folder" "$rootdir/$localdir"
     done
-
     cd "$rootdir"
 }
 
