8cbcf66d4184b2d68e9218e77809291b2ebdac6d
ywt114 <58467878+ywt114@users.noreply.github.com>
2024-05-25 08:20:09 +0800

    Add files via upload

--- diy-part2_6.6.sh
+++ diy-part2_6.6.sh
@@ -134,7 +134,7 @@ sed -i 's/Variable2 = "*.*"/Variable2 = "test"/g' package/lean/luci-app-gpsysupg
 sed -i 's/Variable3 = "*.*"/Variable3 = "x86_64"/g' package/lean/luci-app-gpsysupgrade/root/usr/bin/upgrade.lua
 sed -i 's/Variable4 = "*.*"/Variable4 = "6.6"/g' package/lean/luci-app-gpsysupgrade/root/usr/bin/upgrade.lua
 
- 回退passwall到2024年1月9日旧版本
+# 回退passwall到2024年1月9日旧版本
 cd package/lean/small/luci-app-passwall
 git reset --hard ecd92aa64174f5e7a200678b4122891764a6b245
 cd ../../../..
