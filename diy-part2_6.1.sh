b99bca35701971ccc00f33b9b0ec4b80165723c4
ywt114 <58467878+ywt114@users.noreply.github.com>
2025-12-27 17:08:52 +0800

    Update diy-part2_6.1.sh

--- diy-part2_6.1.sh
+++ diy-part2_6.1.sh
@@ -26,140 +26,157 @@ function merge_package() {
     cd "$rootdir"
 }
 
-# 修改管理地址
+# Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
-# 交换LAN/WAN口
-sed -i 's/"eth1 eth2" "eth0"/"eth1 eth2" "eth0"/g' target/linux/x86/base-files/etc/board.d/02_network
-sed -i "s/'eth1 eth2' 'eth0'/'eth1 eth2' 'eth0'/g" target/linux/x86/base-files/etc/board.d/02_network
-sed -i "s/lan 'eth0'/lan 'eth0'/g" package/base-files/files/etc/board.d/99-default_network
-sed -i "s/wan 'eth1'/wan 'eth1'/g" package/base-files/files/etc/board.d/99-default_network
-sed -i "s/net\/eth1/net\/eth1/g" package/base-files/files/etc/board.d/99-default_network
+# Modify default hostname
+sed -i 's/OpenWrt/ImmortalWrt/g' package/base-files/files/bin/config_generate
 
-# 修改默认皮肤
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' feeds/luci/collections/luci-ssl-nginx/Makefile
+# Modify default timezone
+sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
 
-# 修改主机名以及一些显示信息
-sed -i "s/hostname='*.*'/hostname='test'/" package/base-files/files/bin/config_generate
-sed -i "s/DISTRIB_ID='*.*'/DISTRIB_ID='OpenWrt'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt'/g"  package/base-files/files/etc/openwrt_release
-sed -i '/(<%=pcdata(ver.luciversion)%>)/a\      built by test' package/lean/autocore/files/x86/index.htm
-echo -n "$(date +'%Y%m%d')" > package/base-files/files/etc/openwrt_version
-curl -fsSL https://raw.githubusercontent.com/ywt114/diy/main/banner_test > package/base-files/files/etc/banner
+# Modify default banner
+echo "-----------------------------------------------------" >> package/base-files/files/etc/banner
+echo " ImmortalWrt ${Version} by immortalwrt" >> package/base-files/files/etc/banner
+echo "-----------------------------------------------------" >> package/base-files/files/etc/banner
 
-# 修改部分默认设置
-sed -i "/exit 0/i sed -i '\/oui\/d' \/etc\/opkg\/distfeeds.conf" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/option check_signature/# option check_signature/g" package/system/opkg/Makefile
-# echo "src/gz openwrt_kiddin9 https://op.supes.top/packages/x86_64" >> package/system/opkg/files/customfeeds.conf
-echo "src/gz openwrt_kenzok8 https://op.dllkids.xyz/packages/x86_64" >> package/system/opkg/files/customfeeds.conf
-sed -i "s/mirrors.cloud.tencent.com\/lede/mirrors.cloud.tencent.com\/openwrt/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/sed -i 's\/root::0:0:99999:7:::/# sed -i 's\/root::0:0:99999:7:::/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/sed -i 's\/root:::0:99999:7:::/# sed -i 's\/root:::0:99999:7:::/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/sed -i '\/REDIRECT --to-ports/# sed -i '\/REDIRECT --to-ports/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/echo 'iptables -t/echo '# iptables -t/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/echo '\[ -n/echo '# \[ -n/g" package/lean/default-settings/files/zzz-default-settings
+# Modify default system release
+sed -i "s/DISTRIB_ID='*.*'/DISTRIB_ID='ImmortalWrt'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='ImmortalWrt'/g" package/base-files/files/etc/openwrt_release
 
-# 开启wifi选项
-sed -i 's/disabled=*.*/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/ssid=*.*/ssid=test/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# Modify default opkg source
+sed -i 's/option check_signature/# option check_signature/g' package/system/opkg/files/opkg.conf
+echo "src/gz immortalwrt https://op.supes.top/packages/x86_64" >> package/system/opkg/files/customfeeds.conf
 
-# 添加关机按钮到系统选项
-curl -fsSL https://raw.githubusercontent.com/ywt114/diy/main/poweroff.htm > feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm
-curl -fsSL https://raw.githubusercontent.com/ywt114/diy/main/system.lua > feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 删除替换默认源插件和添加插件
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-# find ./ | grep Makefile | grep pdnsd-alt | xargs rm -f
-# \rm -rf feeds/packages/net/v2ray-geodata feeds/packages/net/pdnsd-alt
-\rm -rf feeds/packages/net/{v2ray-geodata,pdnsd-alt}
-\rm -rf feeds/packages/lang/golang
-git clone -b 24.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
-# rm -rf feeds/packages/net/curl
-# git clone -b main https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
-git clone -b lua https://github.com/sbwml/luci-app-alist package/lean/alist
-\rm -rf feeds/packages/net/mosdns feeds/luci/applications/luci-app-mosdns feeds/packages/utils/v2dat
-git clone -b v5-lua https://github.com/sbwml/luci-app-mosdns package/lean/mosdns
-\rm -rf feeds/luci/applications/luci-app-adbyby-plus
-git clone -b main https://github.com/ywt114/luci-app-adbyby-plus-lite package/lean/luci-app-adbyby-plus-lite
-\rm -rf feeds/packages/net/msd_lite
-git clone -b main https://github.com/ywt114/luci-app-msd_lite package/lean/msd_lite
-git clone -b master https://github.com/ywt114/luci-app-gpsysupgrade package/lean/luci-app-gpsysupgrade
-# \rm -rf feeds/packages/net/smartdns feeds/luci/applications/luci-app-smartdns
-# git clone -b master https://github.com/pymumu/openwrt-smartdns package/lean/smartdns
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
-git clone -b master https://github.com/kenzok8/small package/lean/small
-\rm -rf package/lean/small/{*bypass*,*fchomo*,*homeproxy*,*mihomo*,*mosdns*,*nikki*,*passwall2*}
-sed -i 's/-flto/-flto -Wno-error/g' package/lean/small/shadowsocksr-libev/Makefile
-git clone -b main https://github.com/sirpdboy/luci-app-chatgpt-web package/lean/luci-app-chatgpt-web
-git clone -b master https://github.com/sirpdboy/luci-app-advanced package/lean/luci-app-advanced
-git clone -b master https://github.com/sirpdboy/luci-app-autotimeset package/lean/luci-app-autotimeset
-sed -i 's/control"/system"/g' package/lean/luci-app-autotimeset/luasrc/controller/autotimeset.lua
-sed -i 's/control]/system]/g' package/lean/luci-app-autotimeset/luasrc/view/autotimeset/log.htm
-# git clone -b master https://github.com/kenzok8/openwrt-packages package/lean/openwrt-packages
-# \cp -rf package/lean/openwrt-packages/luci-app-openclash package/lean/small
-# \rm -rf package/lean/openwrt-packages
-\rm -rf feeds/packages/net/socat feeds/luci/applications/luci-app-socat
-git clone -b master https://github.com/xiangfeidexiaohuo/extra-ipk package/lean/extra-ipk
-\cp -rf package/lean/extra-ipk/op-socat package/lean/socat
-\cp -rf package/lean/extra-ipk/op-homebox package/lean/homebox
-\rm -rf package/lean/extra-ipk
-\rm -rf feeds/packages/net/adguardhome feeds/luci/applications/luci-app-adguardhome
-git clone -b main https://github.com/sirpdboy/sirpdboy-package package/lean/sirpdboy-package
-\cp -rf package/lean/sirpdboy-package/adguardhome package/lean
-\cp -rf package/lean/sirpdboy-package/luci-app-adguardhome package/lean
-\rm -rf package/lean/sirpdboy-package
-git clone -b main https://github.com/linkease/openwrt-app-actions package/lean/openwrt-app-actions
-\cp -rf package/lean/openwrt-app-actions/applications/luci-app-multiaccountdial package/lean
-\rm -rf package/lean/openwrt-app-actions
-git clone -b main https://github.com/linkease/istore package/lean/istore
-# sed -i 's/+luci-lib-ipkg/+luci-base/g' package/lean/istore/luci/luci-app-store/Makefile
-\cp -rf package/lean/istore/luci/* package/lean
-\cp -rf package/lean/istore/translations package/lean
-\rm -rf package/lean/istore
-git clone -b main https://github.com/linkease/nas-packages-luci package/lean/nas-packages-luci
-sed -i 's/istorex_template")).leaf/istorex_template"), _("iStoreX"), 3).leaf/g' package/lean/nas-packages-luci/luci/luci-app-istorex/luasrc/controller/istorex.lua
-\cp -rf package/lean/nas-packages-luci/luci/* package/lean
-\rm -rf package/lean/nas-packages-luci
-git clone -b master https://github.com/linkease/nas-packages package/lean/nas-packages
-\cp -rf package/lean/nas-packages/network/services/* package/network/services
-\cp -rf package/lean/nas-packages/multimedia package
-\rm -rf package/lean/nas-packages
-# sed -i 's/fw.koolcenter.com\/binary\/ddnsto/github.com\/ywt114\/diy\/releases\/download\/services/g' package/network/services/ddnsto/Makefile
-# sed -i 's/fw.koolcenter.com\/binary\/iStoreEnhance/github.com\/ywt114\/diy\/releases\/download\/services/g' package/network/services/istoreenhance/Makefile
-# sed -i 's/fw0.koolcenter.com\/binary\/LinkEase\/LinuxStorage/github.com\/ywt114\/diy\/releases\/download\/services/g' package/network/services/linkease/Makefile
-# sed -i 's/fw0.koolcenter.com\/binary\/LinkEase\/AutoUpgrade\/linkmount/github.com\/ywt114\/diy\/releases\/download\/services/g' package/network/services/linkmount/Makefile
-git clone -b main https://github.com/Carseason/openwrt-packages package/lean/openwrt-packages
-\cp -rf package/lean/openwrt-packages/network/services/* package/network/services
-\cp -rf package/lean/openwrt-packages/luci/luci-app-istorego package/lean
-\cp -rf package/lean/openwrt-packages/luci/luci-app-routerdog package/lean
-\rm -rf package/lean/openwrt-packages
+# Modify default services
+sed -i '/exit 0/i\# Start MosDNS' package/base-files/files/etc/rc.local
+sed -i '/exit 0/i\mosdns start' package/base-files/files/etc/rc.local
+sed -i '/exit 0/i\# Start SmartDNS' package/base-files/files/etc/rc.local
+sed -i '/exit 0/i\smartdns start' package/base-files/files/etc/rc.local
 
-# 创建automake-1.15软连接
-# sudo -E ln -sf /usr/bin/automake /usr/bin/automake-1.15
+# Modify default firewall
+sed -i 's/option drop_invalid/option drop_invalid 1/g' package/network/config/firewall/files/firewall.config
 
-# 取消部分config配置
-sed -i 's/iperf3-ssl[[:space:]]*//g' target/linux/x86/Makefile
-# sed -i '/CONFIG_PACKAGE_kmod-usb-audio/d' ./.config
-# echo "# CONFIG_PACKAGE_kmod-usb-audio is not set" >> ./.config
-# echo "# CONFIG_PACKAGE_kmod-media-core is not set" >> ./.config
+# Modify default wireless
+sed -i 's/option disabled 1/option disabled 0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 修改vermagic版本号
-# curl -fsSL https://raw.githubusercontent.com/ywt114/diy/main/vermagic-6.1 > vermagic
-# sed -i 's/grep '\''=\[ym\]'\'' $(LINUX_DIR)\/.config.set | LC_ALL=C sort | $(MKHASH) md5 >/cp $(TOPDIR)\/vermagic/g' include/kernel-defaults.mk
-# sed -i 's/$(SCRIPT_DIR)\/kconfig.pl $(LINUX_DIR)\/.config | $(MKHASH) md5/cat $(LINUX_DIR)\/.vermagic/g' package/kernel/linux/Makefile
+# Modify default packages
+sed -i 's/CONFIG_PACKAGE_luci-app-adbyby-plus=y/CONFIG_PACKAGE_luci-app-adblock=y/g' .config
+sed -i 's/CONFIG_PACKAGE_luci-theme-argon-mod=y/CONFIG_PACKAGE_luci-theme-argon=y/g' .config
+sed -i 's/CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y/CONFIG_PACKAGE_dnsmasq-full=y/g' .config
+sed -i 's/# CONFIG_PACKAGE_odhcpd-ipv6only is not set/CONFIG_PACKAGE_odhcpd-ipv6only=y/g' .config
 
-sed -i 's/Variable1 = "*.*"/Variable1 = "ywt114"/g' package/lean/luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
-sed -i 's/Variable2 = "*.*"/Variable2 = "test"/g' package/lean/luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
-sed -i 's/Variable3 = "*.*"/Variable3 = "x86_64"/g' package/lean/luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
-sed -i 's/Variable4 = "*.*"/Variable4 = "6.1"/g' package/lean/luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
-sed -i 's/Variable1 = "*.*"/Variable1 = "ywt114"/g' package/lean/luci-app-gpsysupgrade/root/usr/bin/upgrade.lua
-sed -i 's/Variable2 = "*.*"/Variable2 = "test"/g' package/lean/luci-app-gpsysupgrade/root/usr/bin/upgrade.lua
-sed -i 's/Variable3 = "*.*"/Variable3 = "x86_64"/g' package/lean/luci-app-gpsysupgrade/root/usr/bin/upgrade.lua
-sed -i 's/Variable4 = "*.*"/Variable4 = "6.1"/g' package/lean/luci-app-gpsysupgrade/root/usr/bin/upgrade.lua
+# Modify default kernel
+sed -i 's/CONFIG_KERNEL_BUILD_USER="*.*"/CONFIG_KERNEL_BUILD_USER="immortalwrt"/g' .config
+sed -i 's/CONFIG_KERNEL_BUILD_DOMAIN="*.*"/CONFIG_KERNEL_BUILD_DOMAIN="immortalwrt"/g' .config
 
-# 回退v2ray-plugin到2024年08月24日旧版本
-# cd package/lean/small/v2ray-plugin
-# git reset --hard 82eac7940f75b6ed59523c728adca179ab001aaf
-# cd ../../../..
+# Modify default grub
+sed -i 's/CONFIG_GRUB_TITLE="*.*"/CONFIG_GRUB_TITLE="ImmortalWrt by immortalwrt"/g' .config
+
+# Modify default rootfs size
+sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=*.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=600/g' .config
+
+# Modify default feeds
+sed -i 's/openwrt\/packages/immortalwrt\/packages/g' feeds.conf.default
+sed -i 's/openwrt\/luci/immortalwrt\/luci/g' feeds.conf.default
+sed -i 's/openwrt\/routing/immortalwrt\/routing/g' feeds.conf.default
+sed -i 's/openwrt\/telephony/immortalwrt\/telephony/g' feeds.conf.default
+
+# Modify default banner file
+curl -fsSL https://raw.githubusercontent.com/immortalwrt/diy/master/banner_IMMORTALWRT > package/base-files/files/etc/banner
+
+# Modify default luci-app-zerotier
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-zerotier/root/etc/config/zerotier
+
+# Modify default luci-app-upnp
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-upnp/root/etc/config/upnpd
+
+# Modify default luci-app-wol
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-wol/root/etc/config/wol
+
+# Modify default luci-app-ddns
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-ddns/root/etc/config/ddns
+
+# Modify default luci-app-samba4
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-samba4/root/etc/config/samba4
+
+# Modify default luci-app-turboacc
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-turboacc/root/etc/config/turboacc
+
+# Modify default luci-app-mosdns
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-mosdns/root/etc/config/mosdns
+
+# Modify default luci-app-smartdns
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-smartdns/root/etc/config/smartdns
+
+# Modify default luci-app-adblock
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-adblock/root/etc/config/adblock
+
+# Modify default luci-app-firewall
+sed -i 's/option enabled 0/option enabled 1/g' package/network/config/firewall/files/firewall.config
+
+# Modify default luci-app-accesscontrol
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-accesscontrol/root/etc/config/accesscontrol
+
+# Modify default luci-app-autotimeset
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-autotimeset/root/etc/config/autotimeset
+
+# Modify default luci-app-chatgpt-web
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-chatgpt-web/root/etc/config/chatgpt-web
+
+# Modify default luci-app-ddnsto
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-ddnsto/root/etc/config/ddnsto
+
+# Modify default luci-app-diskman
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-diskman/root/etc/config/diskman
+
+# Modify default luci-app-dockerman
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-dockerman/root/etc/config/dockerman
+
+# Modify default luci-app-homebox
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-homebox/root/etc/config/homebox
+
+# Modify default luci-app-msd_lite
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-msd_lite/root/etc/config/msd_lite
+
+# Modify default luci-app-multiaccountdial
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-multiaccountdial/root/etc/config/multiaccountdial
+
+# Modify default luci-app-mwan3
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-mwan3/root/etc/config/mwan3
+
+# Modify default luci-app-netdata
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-netdata/root/etc/config/netdata
+
+# Modify default luci-app-nlbwmon
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-nlbwmon/root/etc/config/nlbwmon
+
+# Modify default luci-app-ramfree
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-ramfree/root/etc/config/ramfree
+
+# Modify default luci-app-socat
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-socat/root/etc/config/socat
+
+# Modify default luci-app-ttyd
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-ttyd/root/etc/config/ttyd
+
+# Modify default luci-app-udpxy
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-udpxy/root/etc/config/udpxy
+
+# Modify default luci-app-unblockmusic
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-unblockmusic/root/etc/config/unblockmusic
+
+# Modify default luci-app-unishare
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-unishare/root/etc/config/unishare
+
+# Modify default luci-app-vlmcsd
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-vlmcsd/root/etc/config/vlmcsd
+
+# Modify default luci-app-wireguard
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-wireguard/root/etc/config/wireguard
+
+# Modify default luci-app-zerotier
+sed -i 's/option enabled 0/option enabled 1/g' feeds/luci/applications/luci-app-zerotier/root/etc/config/zerotier
